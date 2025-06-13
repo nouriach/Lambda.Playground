e56f0a4b294b4ec571e8ab2aa60f6f52c3984dd2
nouriach <nouriach17@gmail.com>
2025-06-13 12:24:30 +0100

    Setup IAM policies & roles
    In Terraform and AWS IAM, `aws_iam_policy_document`,
    `aws_iam_policy`, and `aws_iam_role_policy_attachment`
    are often used together to define and apply permissions
    to roles.
    
    `aws_iam_policy_document` = builds the actual JSON string
    that will be used as the policy document.
    
    `aws_iam_policy` = converts the output of `aws_iam_policy_document`
    into a reusable, named IAM policy that lives in AWS.
    
    `aws_iam_role_policy_attachment` = grants the role the permissions
    defined in the policy.

---
+++ terraform/iam.tf
@@ -0,0 +1,46 @@
+# Build the Policy Documents: what can these policies/roles do?
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+    actions = [
+      "sts:AssumeRole"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "allow_logging" {
+  policy_id = "AllowLambdaPushLog"
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = ["arn:aws:logs:*:*:*"] <--- update
+  }
+}
+
+# Create an IAM Role for the Lambda. Its rules are defined in the Policy Document.
+resource "aws_iam_role" "lambda_iam_role" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
+
+# Create an IAM Policy for Logging. Its rules are defined in the Policy Document.
+resource "aws_iam_policy" "lambda_logging" {
+  name   = "lambda-logging-cloudwatch"
+  policy = data.aws_iam_policy_document.allow_logging.json
+}
+
+# Attach the policy to the IAM Role
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_iam_role.id
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
